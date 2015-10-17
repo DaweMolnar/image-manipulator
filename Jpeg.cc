bee58cc98df4259c7d053293747ce8480ff17d9c
Decesso <dawemolnar@gmail.com>
2015-10-17 20:43:19 +0200

    Modified header reading

--- Jpeg.cc
+++ Jpeg.cc
@@ -5,13 +5,12 @@ void
 Jpeg::readImage(const std::string& fileName)
 {
   std::ifstream ifp;
-  JpegHeader header;
   ifp.open(fileName.c_str(), std::ifstream::in | std::ifstream::binary);
   if(!ifp) {
 	throw std::runtime_error("Can't read image file.");
   }
-  ifp.read((char*)&header,sizeof(JpegHeader));
-  if(header.marker[0]!=0xFF || header.marker[1] != 0xD8) {
+  ifp.read((char*)&header_,sizeof(JFIFHEAD));
+  if(header_.SOI[0]!=0xFF || header_.SOI[1] != 0xD8) {
 	throw std::runtime_error("File is not a JPEG file");
   }
 }
--- Jpeg.hh
+++ Jpeg.hh
@@ -3,17 +3,27 @@
 
 #include <string>
 
+typedef struct _JFIFHeader
+{
+  unsigned char SOI[2];			/* 00h  Start of Image Marker		*/
+  unsigned char APP0[2];		/* 02h  Application Use Marker		*/
+  unsigned char Length[2];		/* 04h  Length of APP0 Field		*/
+  unsigned char Identifier[5];	/* 06h  "JFIF" (zero terminated) Id String */
+  unsigned char Version[2];		/* 07h  JFIF Format Revision		*/
+  unsigned char Units;			/* 09h  Units used for Resolution	*/
+  unsigned char Xdensity[2];	/* 0Ah  Horizontal Resolution		*/
+  unsigned char Ydensity[2];	/* 0Ch  Vertical Resolution			*/
+  unsigned char XThumbnail;		/* 0Eh  Horizontal Pixel Count		*/
+  unsigned char YThumbnail;		/* 0Fh  Vertical Pixel Count		*/
+} JFIFHEAD;
+
 class Jpeg {
 public:
   Jpeg() {}
   ~Jpeg() {}
   void readImage(const std::string& fileName);
 private:
-  struct JpegHeader {
-	unsigned char marker[2];
-	unsigned char width[2];
-	unsigned char height[2];
-  };
+  JFIFHEAD header_;
 };
 
 #endif //JPEG_HH
